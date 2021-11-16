976562908ddcbf8a53dbfb50eb313a37af5b3b32
Ryan Brouwer <mennofromheavan@gmail.com>
2021-11-16 21:37:02 +0000

    please excuse the styling, just messing arond

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Article.create(title:     Faker::Book.title,
+                content:    Faker::Lorem.paragraph(sentence_count: 8),
+                author:     Faker::Name.name_with_middle,
+                published:  Faker::Date.backward(days: 200))
+end
